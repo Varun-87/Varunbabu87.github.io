4a3da23bcef5dacb57317b697095ab295d6d4fef
Varun-87 <101583593+Varun-87@users.noreply.github.com>
2025-12-21 11:17:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleCard(card) {
   const details = card.querySelector(".details");
   details.style.display = details.style.display === "block" ? "none" : "block";
 }
+function openImage() {
+  document.getElementById("imageModal").style.display = "flex";
+}
+
+function closeImage() {
+  document.getElementById("imageModal").style.display = "none";
+}
